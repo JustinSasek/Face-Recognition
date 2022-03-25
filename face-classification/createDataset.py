f90ee6952f225890b0c80ac5dc3dfcc70f748c42
root <root@pop-os.localdomain>
2022-03-24 21:37:44 -0500

    updated so that its easier/faster to create dataset !!! hotkeyLoop() vs manualLoop() !!!

--- face-classification/createDataset.py
+++ face-classification/createDataset.py
@@ -1,67 +1,90 @@
 from cv2 import VideoCapture, imwrite
 import os
+import keyboard
 
 
 WORKING_DIR = '.'
 DATASET_NAME = 'eclair-faces'
-
-root = os.path.join(WORKING_DIR, DATASET_NAME)
-img = os.path.join(root, 'img')
-idFile = os.path.join(root, 'id.txt')
-sizeFile = os.path.join(root, 'size.txt')
+FILENAME_WIDTH = 16
 
 
-def mkdir(path):
-    try:
-        os.mkdir(path)
-        print(f'Created {path}')
-    except OSError:
-        print(f'{path} already exists...')
+class DatasetCreator:
+    STOP_HOTKEY = 'space'  # hotkey to stop recording
 
+    def __init__(self, working_dir='.', dataset_name='eclair-faces', filename_width=16):
+        self.root = os.path.join(WORKING_DIR, DATASET_NAME)
+        self.img = os.path.join(self.root, 'img')
+        self.idFile = os.path.join(self.root, 'id.txt')
+        self.sizeFile = os.path.join(self.root, 'size.txt')
+        self.cam = VideoCapture(0)  # TODO: FIND UR WEBCAM !!!!! !  !! !
 
+        self.createDirs()
+        with open(self.sizeFile, 'r') as f:
+            self.size = int(f.read().strip())
 
-def createDirs():
-    mkdir(root)
-    mkdir(img)
+    def mkdir(self, path):
+        try:
+            os.mkdir(path)
+            print(f'Created {path}')
+        except OSError:
+            print(f'{path} already exists...')
+        os.chmod(path, 0o777)
 
-    try:
-        with open(idFile, 'x') as f:
-            print(f'Created {idFile}')
-    except FileExistsError:
-        print(f'{idFile} already exists...')
+    def mkfile(self, path, init=None):  # init is what to initialize file with
+        try:
+            with open(path, 'x') as f:
+                if init is not None:
+                    f.write(str(init))
+                print(f'Created {path}')
+        except FileExistsError:
+            print(f'{path} already exists...')
+        os.chmod(path, 0o777)
 
-    try:
-        with open(sizeFile, 'x') as f:
-            f.write('0')
-            print(f'Created {sizeFile}')
-    except FileExistsError:
-        print(f'{sizeFile} already exists...')
+    def createDirs(self):
+        self.mkdir(self.root)
+        self.mkdir(self.img)
+        self.mkfile(self.idFile)
+        self.mkfile(self.sizeFile, init='0')  # initialize file with 0 if it does not exist
 
-
-def cameraLoop():
-    cam = VideoCapture(1)
-    with open(sizeFile, 'r') as f:
-        size = int(f.read().strip())
-
-    while True:
-        imgId = input()
-
-        if imgId == '':
-            break
-
-        newImgPath = os.path.join(img, f'{str(size).zfill(16)}.png')
-        result, image = cam.read()
+    def captureData(self, imgId):
+        newImgPath = os.path.join(self.img, f'{str(self.size).zfill(FILENAME_WIDTH)}.png')
+        result, image = self.cam.read()
 
         imwrite(str(newImgPath), image)
-        with open(idFile, 'a') as f:
-            f.write(f'{str(size).zfill(16)}.png {imgId.strip()}')
+        os.chmod(newImgPath, 0o777)
+        with open(self.idFile, 'a') as f:
+            f.write(f'{str(self.size).zfill(FILENAME_WIDTH)}.png {imgId}')
             f.write('\n')
 
-        size += 1
-        with open(sizeFile, 'w') as f:
-            f.write(str(size))
+        self.size += 1
+        with open(self.sizeFile, 'w') as f:
+            f.write(str(self.size))
+
+    def manualLoop(self):
+        while True:
+            imgId = input('Image label (id): ').strip()
+
+            if imgId == '':
+                break
+
+            self.captureData(imgId)
+
+    def hotkeyLoop(self):
+        while True:
+            imgId = input('Image label (id): ').strip()
+
+            if imgId == '':
+                break
+            print(f'Starting at {str(self.size).zfill(FILENAME_WIDTH)}.png, taking images as fast as possible until {self.STOP_HOTKEY} is pressed...')
+
+            while True:
+                if keyboard.is_pressed(self.STOP_HOTKEY):
+                    print(f'Stopped at {str(self.size-1).zfill(FILENAME_WIDTH)}.png')
+                    break
+
+                self.captureData(imgId)
 
 
-createDirs()
+creator = DatasetCreator(WORKING_DIR, DATASET_NAME, FILENAME_WIDTH)
 
-cameraLoop()
+creator.hotkeyLoop()
