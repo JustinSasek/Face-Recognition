08690877067ef4a9d70a09788fc53c693112e481
d0rb <hacperu2010@gmail.com>
2022-03-26 17:55:40 -0500

    omg updated face dataloader and main and model !

---
+++ face-classification/main.py
@@ -0,0 +1,19 @@
+import time
+import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from faceDataloader import data
+from model import model
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    return model
+
+
+if __name__ == '__main__':
+    print("Training")
+    # train_model(model, criterion, optimizer, exp_lr_scheduler, num_epochs=25)
